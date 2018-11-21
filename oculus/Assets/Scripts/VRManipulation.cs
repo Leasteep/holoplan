d22c5962aea50d12cc1c1fca4fda7084746228c7
6steep <6steep@informatik.uni-hamburg.de>
2018-11-21 17:01:19 +0100

    first commit oculus

---
+++ oculus/Assets/Scripts/VRManipulation.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// collider Player ? 
+public class VRManipulation : MonoBehaviour {
+
+    public float MoveStrength = 1;
+    public float ScaleStrength = 1;
+
+    private GameObject _leftController;
+    private GameObject _rightController;
+
+    private GameObject _leftControllerManipObject;
+    private GameObject _rightControllerManipObject;
+
+    private bool _leftControllerPressed;
+    private bool _rightControllerPressed;
+
+    private Vector3 _lastLeftControllerPos;
+    private Vector3 _lastRightControllerPos;
+
+    private Vector3 _lastDistanceBetweenController;
+
+    // Use this for initialization
+    void Start () {
+        _leftControllerPressed = false;
+        _rightControllerPressed = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(_leftControllerPressed && !_rightControllerPressed)
+        {
+            Vector3 controllerPosDelta = _lastLeftControllerPos - _leftController.transform.position;
+
+            _leftControllerManipObject.transform.position -= MoveStrength * new Vector3(controllerPosDelta.x, 0, controllerPosDelta.z);
+
+            _lastLeftControllerPos = _leftController.transform.position;
+        }
+        else if(!_leftControllerPressed && _rightControllerPressed)
+        {
+            Vector3 controllerPosDelta = _lastRightControllerPos - _rightController.transform.position;
+            
+            _rightControllerManipObject.transform.position -= MoveStrength * new Vector3(controllerPosDelta.x, 0, controllerPosDelta.z);
+
+            _lastRightControllerPos = _rightController.transform.position;
+        }
+        else if(_leftControllerPressed && _rightControllerPressed)
+        {
+            if(_leftControllerManipObject == _rightControllerManipObject)
+            {
+                Vector3 DistanceBetweenController = _leftController.transform.position - _rightController.transform.position;
+                float scale = _lastDistanceBetweenController.magnitude - DistanceBetweenController.magnitude;
+                float rot = Vector3.SignedAngle(_lastDistanceBetweenController.normalized, DistanceBetweenController.normalized, Vector3.up);
+
+
+                /*
+                Vector3 LeftcontrollerPosDelta = _lastLeftControllerPos - _leftController.transform.position;
+                Vector3 RightcontrollerPosDelta = _lastRightControllerPos - _rightController.transform.position;
+
+                float xPos;
+                float zPos;
+                if((LeftcontrollerPosDelta.x < 0) ^ (RightcontrollerPosDelta.x < 0))
+                {
+                    xPos = LeftcontrollerPosDelta.x + RightcontrollerPosDelta.x;
+                }
+                else
+                {
+                    xPos = Mathf.Abs(LeftcontrollerPosDelta.x - RightcontrollerPosDelta.x);
+                }
+
+                if ((LeftcontrollerPosDelta.z < 0) ^ (RightcontrollerPosDelta.z < 0))
+                {
+                    zPos = LeftcontrollerPosDelta.z + RightcontrollerPosDelta.z;
+                }
+                else
+                {
+                    zPos = Mathf.Abs(LeftcontrollerPosDelta.z - RightcontrollerPosDelta.z);
+                }
+
+                _lastLeftControllerPos = _leftController.transform.position;
+                _lastRightControllerPos = _rightController.transform.position;
+                _leftControllerManipObject.transform.position -= new Vector3(xPos, 0, zPos);
+
+                */
+
+                // für rot und scale, map anchor auf die pos zwischen den beiden controllern bewegen, dabei die map (child) zurückbewegen (negative bewegung wie die anchor bewegung) und erst dann den Anchor rotieren/skalieren
+                _leftControllerManipObject.transform.rotation = _leftControllerManipObject.transform.rotation * Quaternion.Euler(0, rot, 0);
+                _leftControllerManipObject.transform.localScale += new Vector3(1, 0, 1) * scale * ScaleStrength;
+                _lastDistanceBetweenController = DistanceBetweenController;
+            }
+        }
+	}
+
+    public void ControllerPressed(GameObject controller, string ControllerTag, GameObject manipulatedObject)
+    {
+        if(ControllerTag == "leftController")
+        {
+            _leftController = controller;
+            _leftControllerManipObject = manipulatedObject;
+            _leftControllerPressed = true;
+            _lastLeftControllerPos = controller.transform.position;
+        }
+        else if(ControllerTag == "rightController")
+        {
+            _rightController = controller;
+            _rightControllerManipObject = manipulatedObject;
+            _rightControllerPressed = true;
+            _lastRightControllerPos = controller.transform.position;
+        }
+        if(_leftControllerPressed && _rightControllerPressed)
+        {
+            _lastDistanceBetweenController = _leftController.transform.position - _rightController.transform.position;
+        }
+    }
+
+    public void ControllerUnpressed(string ControllerTag)
+    {
+        if (ControllerTag == "leftController")
+        {
+            _leftControllerPressed = false;
+        }
+        else if (ControllerTag == "rightController")
+        {
+            _rightControllerPressed = false;
+        }
+    }
+}
---
+++ oculus/Assets/Scripts/VRManipulative.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRManipulative : MonoBehaviour {
+
+    public VRManipulation VRManip;
+
+    private bool _pressed;
+	// Use this for initialization
+	void Start () {
+        _pressed = false;
+	}
+
+    private void Update()
+    {
+
+        if (gameObject.tag == "leftController")
+        {
+            if (!OVRInput.Get(OVRInput.Button.Three) && _pressed)
+            {
+                ControllerUnpressed();
+            }
+        }
+        else if (gameObject.tag == "rightController")
+        {
+            if (!OVRInput.Get(OVRInput.Button.One) && _pressed)
+            {
+                ControllerUnpressed();
+            }
+        }
+    }
+
+    private void ControllerUnpressed()
+    {
+        _pressed = false;
+        VRManip.ControllerUnpressed(gameObject.tag);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (gameObject.tag == "leftController")
+        {
+            if (OVRInput.Get(OVRInput.Button.Three) && !_pressed)
+            {
+                _pressed = true;
+                VRManip.ControllerPressed(gameObject, gameObject.tag, other.gameObject);
+            }
+        }
+        else if (gameObject.tag == "rightController")
+        {
+            if (OVRInput.Get(OVRInput.Button.One) && !_pressed)
+            {
+                _pressed = true;
+                VRManip.ControllerPressed(gameObject, gameObject.tag, other.gameObject);
+            }
+        }
+    }
+}
+
