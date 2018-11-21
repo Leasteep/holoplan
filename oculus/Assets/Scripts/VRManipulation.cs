17277784265a8925d0c36e86985743719ad3bb8f
6steep <6steep@informatik.uni-hamburg.de>
2018-11-21 20:46:01 +0100

    set anchor for rotation

--- oculus/Assets/Scripts/VRManipulation.cs
+++ oculus/Assets/Scripts/VRManipulation.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 // collider Player ? 
 public class VRManipulation : MonoBehaviour {
 
+    
     public float MoveStrength = 1;
     public float ScaleStrength = 1;
 
+   
     private GameObject _leftController;
     private GameObject _rightController;
 
     private GameObject _leftControllerManipObject;
     private GameObject _rightControllerManipObject;
+    private GameObject _mapAnchor;
 
     private bool _leftControllerPressed;
     private bool _rightControllerPressed;
@@ -21,20 +24,26 @@ public class VRManipulation : MonoBehaviour {
     private Vector3 _lastRightControllerPos;
 
     private Vector3 _lastDistanceBetweenController;
+    private Vector3 _lastPositionAnchor;
 
     // Use this for initialization
     void Start () {
+
+       // Physics.IgnoreCollision(control, _leftControllerManipObject);
         _leftControllerPressed = false;
         _rightControllerPressed = false;
+        _mapAnchor = GameObject.Find("/MapAnchor");
     }
 	
 	// Update is called once per frame
-	void Update () {
-		if(_leftControllerPressed && !_rightControllerPressed)
+	void Update (){
+        
+        
+        if (_leftControllerPressed && !_rightControllerPressed)
         {
             Vector3 controllerPosDelta = _lastLeftControllerPos - _leftController.transform.position;
 
-            _leftControllerManipObject.transform.position -= MoveStrength * new Vector3(controllerPosDelta.x, 0, controllerPosDelta.z);
+            _leftControllerManipObject.transform.position -= MoveStrength * controllerPosDelta;
 
             _lastLeftControllerPos = _leftController.transform.position;
         }
@@ -42,7 +51,7 @@ public class VRManipulation : MonoBehaviour {
         {
             Vector3 controllerPosDelta = _lastRightControllerPos - _rightController.transform.position;
             
-            _rightControllerManipObject.transform.position -= MoveStrength * new Vector3(controllerPosDelta.x, 0, controllerPosDelta.z);
+            _rightControllerManipObject.transform.position -= MoveStrength * controllerPosDelta;
 
             _lastRightControllerPos = _rightController.transform.position;
         }
@@ -54,8 +63,8 @@ public class VRManipulation : MonoBehaviour {
                 float scale = _lastDistanceBetweenController.magnitude - DistanceBetweenController.magnitude;
                 float rot = Vector3.SignedAngle(_lastDistanceBetweenController.normalized, DistanceBetweenController.normalized, Vector3.up);
 
-
-                /*
+ /*
+                
                 Vector3 LeftcontrollerPosDelta = _lastLeftControllerPos - _leftController.transform.position;
                 Vector3 RightcontrollerPosDelta = _lastRightControllerPos - _rightController.transform.position;
 
@@ -83,14 +92,23 @@ public class VRManipulation : MonoBehaviour {
                 _lastRightControllerPos = _rightController.transform.position;
                 _leftControllerManipObject.transform.position -= new Vector3(xPos, 0, zPos);
 
-                */
+             */   
 
                 // für rot und scale, map anchor auf die pos zwischen den beiden controllern bewegen, dabei die map (child) zurückbewegen (negative bewegung wie die anchor bewegung) und erst dann den Anchor rotieren/skalieren
-                _leftControllerManipObject.transform.rotation = _leftControllerManipObject.transform.rotation * Quaternion.Euler(0, rot, 0);
-                _leftControllerManipObject.transform.localScale += new Vector3(1, 0, 1) * scale * ScaleStrength;
+                // set map anchor on position 
+
+                _mapAnchor.transform.position = _leftController.transform.position + DistanceBetweenController/2;
+                 Vector3 MovementAnchor = _lastPositionAnchor - _mapAnchor.transform.position;
+                _leftControllerManipObject.transform.position += MovementAnchor;
+
+                _mapAnchor.transform.rotation = _mapAnchor.transform.rotation * Quaternion.Euler(0, rot, 0);
+                _mapAnchor.transform.localScale -= new Vector3(1, 0, 1) * scale * ScaleStrength;
                 _lastDistanceBetweenController = DistanceBetweenController;
+                //_lastLeftControllerPos = _leftController.transform.position;
+                _lastPositionAnchor = _mapAnchor.transform.position;
             }
         }
+        
 	}
 
     public void ControllerPressed(GameObject controller, string ControllerTag, GameObject manipulatedObject)
@@ -101,6 +119,7 @@ public class VRManipulation : MonoBehaviour {
             _leftControllerManipObject = manipulatedObject;
             _leftControllerPressed = true;
             _lastLeftControllerPos = controller.transform.position;
+            
         }
         else if(ControllerTag == "rightController")
         {
@@ -112,6 +131,7 @@ public class VRManipulation : MonoBehaviour {
         if(_leftControllerPressed && _rightControllerPressed)
         {
             _lastDistanceBetweenController = _leftController.transform.position - _rightController.transform.position;
+            _lastPositionAnchor = _mapAnchor.transform.position;
         }
     }
 
